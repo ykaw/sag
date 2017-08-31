628efdcf7ef2eab7a25a67c8039ff67c27faf337
kaw <kaw>
2017-08-31 11:23:45 +0000

    enhanced dispatching status

--- plot/examples/gen0005timeofs.pl
+++ plot/examples/gen0005timeofs.pl
@@ -11,11 +11,6 @@ $SYNC     = 3;                  # reading data of sync
 
 $stat    = $NOTNTP;             # initial state
 
-# initialize unsynced variables
-$dly = 0;
-$jit = 0;
-$peercnt = 0;
-
 while (<>) {
   chop;
   s/#.*//;
@@ -26,37 +21,44 @@ while (<>) {
   @F = split;
 
   if ($stat == $NOTNTP) {
-    if    ($F[0] eq '=dt')     { $dt = sprintf("%04d/%02d/%02d %02d:%02d", @F[1.. 5]) }
-    elsif ($F[0] eq '=gap')    { print "\n" }
-    elsif ($F[0] eq '=ntpctl') { $stat = $HDRFOUND }
-  } else {
-    if ($stat == $HDRFOUND) {
-      if ($F[4] eq 'unsynced,') {
-        $ofs = 0 + $F[8];
-        $stat  = $UNSYNC;
-      } elsif ($F[0] eq '*') {
-        $ofs = 0 + $F[6];
-        $dly = 0 + $F[7];
-        $jit = 0 + $F[8];
-        $stat = $SYNC;
+    if ($F[0] eq '=dt') {
+      $dt = sprintf("%04d/%02d/%02d %02d:%02d", @F[1.. 5]);
+    } elsif ($F[0] eq '=gap') {
+      print "\n";
+    } elsif ($F[0] eq '=ntpctl') {
+      $stat = $HDRFOUND;
+    }
+  } elsif ($stat == $HDRFOUND) {
+    if ($F[4] eq 'unsynced,') {
+      $ofs = 0 + $F[8];
+      $dly = 0;
+      $jit = 0;
+      $peercnt = 0;
+      $stat  = $UNSYNC;
+    } elsif ($F[0] eq '*') {
+      $ofs = 0 + $F[6];
+      $dly = 0 + $F[7];
+      $jit = 0 + $F[8];
+      $stat = $SYNC;
+    }
+  } elsif ($stat == $UNSYNC ) {
+    if ($F[0] =~ /^\d+$/) {
+      $dly += 0 + $F[6];
+      $jit += 0 + $F[7];
+      $peercnt++;
+    } elsif ($F[0] eq '=end') {
+      if (1 <= $peercnt) {
+        printf("%s %f %f %f\n", $dt, $ofs, $dly/$peercnt, $jit/$peercnt);
       }
-    } else {                    # Status should be UNSYNC or SYNC.
-      if ($stat == $UNSYNC && $F[0] =~ /^\d+$/) {
-        $dly += 0 + $F[6];
-        $jit += 0 + $F[7];
-        $peercnt++;
-      }
-      if ($F[0] eq '=end') {
-        if ($stat == $UNSYNC && 0 < $peercnt) {
-          printf("%s %f %f %f\n", $dt, $ofs, $dly/$peercnt, $jit/$peercnt);
-          $dly = 0;
-          $jit = 0;
-          $peercnt = 0;
-        } elsif ($stat == $SYNC) {
-          printf("%s %f %f %f\n", $dt, $ofs, $dly, $jit);
-        }
-        $stat = $NOTNTP;
-      }
+      $dly = 0;
+      $jit = 0;
+      $peercnt = 0;
+      $stat = $NOTNTP;
+    }
+  } elsif ($stat == $SYNC ) {
+    if ($F[0] eq '=end') {
+      printf("%s %f %f %f\n", $dt, $ofs, $dly, $jit);
+      $stat = $NOTNTP;
     }
   }
   # for debug

3d71e8fad65e5cc50374e3f29d5af53b27468f08
kaw <kaw>
2021-10-04 15:22:35 +0000

    less days of store data and display graphs

--- conf/examples/shconf.sh
+++ conf/examples/shconf.sh
@@ -1,6 +1,6 @@
 #  shconf.sh  -  shell variable configuration for SAG
 #
-#  $Id: shconf.sh,v 1.2 2017/01/07 06:10:54 kaw Exp $
+#  $Id: shconf.sh,v 1.3 2021/10/04 15:22:35 kaw Exp $
 
 # categories to process data
 #
@@ -8,13 +8,13 @@ targets='0001la 0001net 0005mem 0100df 0100time 0005timeofs'
 
 # days to store data
 #
-rotate_max=750
+rotate_max=64
 
 # days to display graph
 #
-     span_la=15
-    span_net=15
-    span_mem=15
-     span_df=375
-   span_time=375
-span_timeofs=15
+     span_la=8
+    span_net=8
+    span_mem=8
+     span_df=32
+   span_time=32
+span_timeofs=8

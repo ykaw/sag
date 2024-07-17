5e191e534f89b618553c90db30c08e92bda68178
kaw <kaw>
2024-07-17 16:25:21 +0000

    introduced sag_rc.sh and sag_ticker.sh - cron-independent facilities

---
+++ bin/sag_rc.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+export SAGHOME=/home/kaw/sag
+
+progname=${0##*/}
+   ticker="sag_ticker.sh"
+ticker_re="[s]ag_ticker.sh"  # to ignore pgrep itself
+
+# print error message to stderr
+#
+err () {
+    echo "$progname: $*" >&2
+}
+
+# print error message then exit
+#
+err_exit () {
+    err "$*"
+    exit 1
+}
+
+# check if sag_ticker.sh already running
+#
+is_ticker_run () {
+    pgrep -f "${ticker_re}"
+    return $?
+}
+
+cd $SAGHOME || err_exit "cannot cd to $SAGHOME"
+
+case "$1" in
+    start)
+        ticker_pid=$(is_ticker_run) && err_exit "$ticker is already running"
+        $SAGHOME/bin/sag_ticker.sh >> ${SAGHOME}/sag.log 2>&1 &
+    ;;
+    stop)
+        ticker_pid=$(is_ticker_run) || err_exit "$ticker is not running"
+        kill -HUP $ticker_pid
+        ;;
+    status)
+        if ticker_pid=$(is_ticker_run); then
+            echo "$ticker is running, PID is $ticker_pid"
+        else
+            echo "$ticker is not running"
+        fi
+        ;;
+    *)
+        err "Usage: $progname [start|stop|status]"
+        ;;
+esac
---
+++ bin/sag_ticker.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+# print error message to stderr
+#
+err () {
+    echo "${0##*/}: $*" >&2
+}
+
+# print error message then exit
+#
+err_exit () {
+    err "$*"
+    exit 1
+}
+
+# wait until specified sec
+#
+wait_next_sec () {
+    local min="$(date +%S)"
+    min="${min#0}"  # strip heading zero
+                    # to avoid an error in
+                    # next arith. expansion
+    if ! sleep $((60 + $1 - min)); then
+	err "interval sleep with error"
+        sleep 60  # to avoid sleepless loop
+    fi
+}
+
+# driver for data acquision and processing
+#
+sag_driver () {
+    export DAYTIME=$(date +'%Y %m %d %H %M')
+    set -- $DAYTIME
+    hour="${4#0}"  # strip heading zero
+    min="${5#0}"   # strip heading zero
+
+    commands="./bin/t0001"  # without any condition
+
+    # build a command sequence
+    #
+    [[ $((min % 5)) = 1 ]]      && commands="${commands}; ./bin/t0005"
+    [[ "$min" = "50" ]]         && commands="${commands}; ./bin/t0100"
+    [[ "$hour:$min" = "3:55" ]] && commands="${commands}; ./bin/t2400"
+
+    nice -n 15 /bin/sh -c "$commands"  # exec commands with low priority
+}
+
+# environment check and setup
+#
+cd $SAGHOME || err_exit "cannot cd to $SAGHOME"
+
+export LANG=C
+
+# make gap for data absence
+#
+./bin/addgap
+
+# main loop
+#
+while :; do
+    wait_next_sec 10
+    sag_driver
+done
